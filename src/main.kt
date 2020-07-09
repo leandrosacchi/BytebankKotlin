f57e67a740fb60337001ef909695228d00687d9e
leandro-gft <lpsacchi@gmail.com>
2020-07-09 08:49:44 -0300

    Conhecendo a Orientação a Objetos em Kotlin

--- src/main.kt
+++ src/main.kt
@@ -3,14 +3,79 @@ import kotlin.reflect.typeOf
 fun main(){
     println("Bem vindo ao Bytebank");
 
+    val conta1 = Conta()
+    conta1.titular = "Leandro"
+    conta1.numeroConta = 1000
+    conta1.saldo = 100.0
 
+    val conta2 = Conta()
+    conta2.titular = "Alex"
+    conta2.numeroConta = 1001
+    conta2.saldo = -200.0
+
+//    println("Titular: " + conta1.titular)
+//    println("Numero da conta: " + conta1.numeroConta)
+//    println("Saldo: " + conta1.saldo)
+//
+//    println("Titular: " + conta2.titular)
+//    println("Numero da conta: " + conta2.numeroConta)
+//    println("Saldo: " + conta2.saldo)
+
+    deposita(conta1,50.0)
+}
+
+fun deposita(conta: Conta, valor: Double){
+    conta.saldo += valor
+    println()
+    println("Bem vindo(a), ${conta.titular}")
+    println("Você depositou o valor $valor na sua conta. Seu saldo agora é de ${conta.saldo}")
+}
+
+fun testaCopiasEReferencias() {
+    val conta1 = Conta()
+    conta1.titular = "Leandro"
+    conta1.numeroConta = 1000
+    conta1.saldo = 100.0
+
+//    val conta2 = conta1    ao fazer isso, as duas contas apontarão para o mesmo espaço na memória. Precisa criar uma nova instância
+    val conta2 = Conta()
+    conta2.titular = "Alex"
+    conta2.numeroConta = 1001
+    conta2.saldo = -200.0
+
+    println("Titular: " + conta1.titular)
+    println("Numero da conta: " + conta1.numeroConta)
+    println("Saldo: " + conta1.saldo)
+    //String template recebe a variavel dentro da string
+
+    testaCondicoes(conta1.saldo);
+
+    println("Titular: " + conta2.titular)
+    println("Numero da conta: " + conta2.numeroConta)
+    println("Saldo: " + conta2.saldo)
+    //String template recebe a variavel dentro da string
+
+    testaCondicoes(conta2.saldo);
+}
+
+
+class Conta{
+    var titular: String = ""
+    var numeroConta: Int = 0
+    var saldo = 0.0
+    //var: variavel mutável; val: variável imutável
+    //nao é necessário declarar o tipo da variável
+
+
+}
+
+fun testaLacos() {
     //estruturas de repetição
-    /*
-    for(i in 1..5){
+    for (i in 1..5) {
         println(i)
     }
 
-    for(i in 5 downTo 1 step 2){
+    for (i in 5 downTo 1 step 2) {
         println(i)
     }
 
@@ -18,22 +83,7 @@ fun main(){
     while (i <= 5) {
         println(i)
         i++
-    } */
-
-    val titular: String = "Alex"
-    val numeroConta: Int = 1000
-    var saldo = 0.0
-    saldo -= 100
-    //var: variavel mutável; val: variável imutável
-    // nao é necessário declarar o tipo da variável
-
-    println("Titular: $titular")
-    println("Numero da conta: $numeroConta")
-    println("Saldo: $saldo")
-    //String template recebe a variavel dentro da string
-
-    testaCondicoes(saldo);
-
+    }
 }
 
 fun testaCondicoes(saldo: Double){
